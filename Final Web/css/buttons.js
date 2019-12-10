74815923e7c6b58e9fb7cd5903309438f3606e0f
aaz298 <54714021+aaz298@users.noreply.github.com>
2019-12-10 00:06:02 -0500

    Add files via upload

---
+++ Final Web/css/buttons.js
@@ -0,0 +1,13 @@
+
+function menuPop(){
+  document.getElementById('close').style.display="block";
+  document.getElementById('menu').style.display="none";
+  document.getElementById("expanded_menu").style.display = "block";
+  document.getElementById("homepage").style.display = "none";
+}
+function closePop(){
+  document.getElementById('close').style.display="none";
+  document.getElementById('menu').style.display="block";
+  document.getElementById("expanded_menu").style.display = "none";
+  document.getElementById("homepage").style.display = "block";
+}
